5ed22f4f9e21a31941b5a4b44a9acf937d073d6f
Manu Fernandez <mfernandezfreire@outlook.com>
2020-12-16 15:50:29 +0100

    My first commit

---
+++ Course/Classes & Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -0,0 +1,11 @@
+// Code goes here!
+class Department {
+    name: string;
+
+    constructor(n :string) {
+        this.name = n
+    }
+}
+
+const pepe = new Department('pepe');
+console.log(pepe)
